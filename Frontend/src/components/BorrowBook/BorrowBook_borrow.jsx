cd025d801f7d583efd4bd26c12b5074126f8c137
0zMiafO7zff <kittipan.mtz.062@gmail.com>
2024-02-17 10:32:51 +0700

    aa

--- Frontend/src/components/BorrowBook/BorrowBook_borrow.jsx
+++ Frontend/src/components/BorrowBook/BorrowBook_borrow.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom' 
 
 export default function BorrowBook_borrow() {
   return (
-    <div>BorrowBook_borrow</div>
+    <section className="bb-borrow">
+        <div className="container w-[760px] mx-auto">
+            <div className="btn-menu">
+            {/* <Link to="/" className="border-[1.5px] border-black px-3 font-medium rounded-md">
+              ยืม-คืนหนังสือ
+            </Link> */}
+            <button className="border-[1.5px] border-black px-3 font-medium rounded-md">
+              ข้อมูลสถิติ
+            </button>
+            </div>
+        </div>
+    </section>
   )
 }
